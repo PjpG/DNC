33fe4b3024a3dccf63ff94e93d4e82a6df6d1a82
PjpG <patryk@doppio.jp>
2014-06-26 01:55:13 +0200

    Restyled member edit form.

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,2 +1,10 @@
 module MembersHelper
+  
+  def editing_background_image(member)
+    if member.present?
+      "background: url(" + member.cover_photo.url(:big) + ");"
+    else 
+      "background-color: rgba(0, 0, 0, .8);"
+    end
+  end
 end
