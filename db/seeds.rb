da6b7f6f656811d2a91320efb47f1a7ab98a9ad7
PjpG <patryk@doppio.jp>
2014-09-10 18:32:16 +0200

    Added functionality of admin seeded user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: "patryk@doppio.jp", password: "password", password_confirmation: "password")
